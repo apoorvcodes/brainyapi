1d349f38c68270bf2ad3b0d225254fb8a892792c
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2022-03-04 21:11:35 +0530

    feat: add middlewares

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/apoorvcodes/brainyapi
 
 go 1.17
 
-require github.com/gominima/minima v1.0.2 // indirect
+require (
+	github.com/gominima/cors v1.0.1 // indirect
+	github.com/gominima/middlewares v0.0.0-20220206154521-7f7036903438 // indirect
+	github.com/gominima/minima v1.0.2 // indirect
+	github.com/rs/cors v1.8.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"github.com/gominima/cors"
+	"github.com/gominima/middlewares"
+	"github.com/gominima/minima"
+)
+
 func main() {
-	
+	app := minima.New()
+	crs := cors.New()
+	app.UseRaw(middleware.AllowContentEncoding())
+	app.UseRaw(middleware.DefaultLogger)
+	app.UseRaw(middleware.RequestID)
+	app.Use(crs.AllowAll())
+        app.Get("/", func(res *minima.Response, req *minima.Request) {
+		res.Send("Hello World")
+	})
+	app.Listen(":3000")
 }
